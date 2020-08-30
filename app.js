6e814d135facb4b0aa8338ce2a56c63f69301699
rafael.sochodolosch <rafaelsocho@gmail.com>
2020-08-30 20:36:02 -0300

    alteração da variavel de conexão

--- app.js
+++ app.js
@@ -2,17 +2,24 @@ import mongoose from 'mongoose';
 import express from 'express';
 import { myBankApi } from './routers/myBankApi.js';
 
+const urlConection =
+  'mongodb+srv://' +
+  process.env.USER_DB +
+  ':' +
+  process.env.USER_PWD +
+  '@cluster0.qe3zr.mongodb.net/' +
+  process.env.BASE +
+  '?retryWrites=true&w=majority';
+console.log(urlConection);
+
 //Conecta com o banco.
 (async () => {
   try {
-    //string de conexão mongodb+srv://socho:<password>@cluster0.qe3zr.mongodb.net/<dbname>?retryWrites=true&w=majority
-    await mongoose.connect(
-      'mongodb+srv://socho:socho@cluster0.qe3zr.mongodb.net/bank?retryWrites=true&w=majority',
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-      }
-    );
+    //Conecta com a varivael de conexao
+    await mongoose.connect(urlConection, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log('Conectado com o MongoDB');
   } catch (err) {
     console.log('Erro ao conectar ao mongo db atlas' + err);
